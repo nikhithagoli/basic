e79948975c4e3887f5f115fb08f3ede904e6b9a7
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-01 11:24:55 +0530

    hello world

--- cspp1-practice/m3/helloworld.py
+++ cspp1-practice/m3/helloworld.py
@@ -1,1 +1,6 @@
+# print is a in buil function used to display the output. 
+
+#It prints what ever is with in the paranthesis,
+
+# it follows the order of sequence of operations entered inside the paranthesis
 print("hello world")
