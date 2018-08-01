c6a940142d8944f1e036f770028d6995e54b7841
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-01 16:44:23 +0530

    even_reverse with for

--- cspp1-practice/m3/iterate_even_reverse.py
+++ cspp1-practice/m3/iterate_even_reverse.py
@@ -1,18 +1,13 @@
 #we have to print even number from 10 to 2
 
-i = 10
-
 print("Hello!")
 
-# while is a iterative structure
-#is used to do same task multiple times till the condition is true
+# for is a iterative structure
+#is used to do same task m times 
 
-while i >= 2:
+for i in range(10,1,-2):
 	print(i)
 	
-# to print the next even number we are decrementing i value by 2
-
-	i -= 2
 	
 
 	
