6ae3ba09a2f8d6af6ecc07d88b58fe52228b9717
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-01 16:16:22 +0530

    even

--- cspp1-practice/m3/iterate_even.py
+++ cspp1-practice/m3/iterate_even.py
@@ -1,22 +1,18 @@
-#we have to print even number below n
-
-print("enter value n")
-
-#as input function returns string by default we should cast it to integer
-
-n=int(input())
+#we have to print even number below 10
 
 # i indicates the starting value
 
-i=2
+i = 2
 
 # while is a iterative structure
 #is used to do same task multiple times till the condition is true
 
-while i<=n:
+while i <= 10:
 	print(i)
 	
 # to print the next even number we are incrementing i value by 2
 
-	i+=2
+	i += 2
+	
+print("Goodbye!")
 	
