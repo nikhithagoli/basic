9987b44b07748ceb282674d9dcf805dc756f489a
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-01 12:15:53 +0530

    hello_happy_world

---
+++ cspp1-practice/m3/hello_happy_world.py
@@ -0,0 +1,15 @@
+#happy is a variable which consists of a integer
+
+#input is an in-built function used to read values from user
+
+#input always returns a string
+
+#when we have to work with integers we have to cast it
+
+happy=int(input())
+
+#if is conditional braching
+
+if happy>2:
+#it executes the following when the condition is true 
+	print("helloworld")
