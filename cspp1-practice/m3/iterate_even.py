0a8fc66498d487b28c761984595512736ffd0354
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-01 16:37:34 +0530

    even with for

--- cspp1-practice/m3/iterate_even.py
+++ cspp1-practice/m3/iterate_even.py
@@ -2,17 +2,12 @@
 
 # i indicates the starting value
 
-i = 2
+# for is a iterative structure
+#is used to do same task n times 
 
-# while is a iterative structure
-#is used to do same task multiple times till the condition is true
-
-while i <= 10:
+for i in range(2,11,2):
 	print(i)
 	
-# to print the next even number we are incrementing i value by 2
-
-	i += 2
 	
 print("Goodbye!")
 	
