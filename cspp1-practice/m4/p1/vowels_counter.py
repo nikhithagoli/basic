9b92fab6571e7662d76a1f2c495de9c6e70be753
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-02 18:37:07 +0530

    " -> 4 of 4 passed. pylint: 10.00/10 "

--- cspp1-practice/m4/p1/vowels_counter.py
+++ cspp1-practice/m4/p1/vowels_counter.py
@@ -1,12 +1,8 @@
-"""#Assume s is a string of lower case characters."""
-def main():
-    inp_s = input()
-    vowel_count = 0
-    for i in inp_s:
-        if i in 'aeiou':
-            vowel_count += 1
-    print(vowel_count)
-
-
-if __name__ == "__main__":
-    main()
+'"# find number of vowels in a given string" '
+S = input()
+C = 0
+'"# counting the vowels" '
+for i in S:
+    if i in 'aeiou':
+        C += 1
+print(C)
