df6de2369fd3bba4e1254cd235181a807b27ea91
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-03 17:41:23 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

--- cspp1-practice/m5/p2/square_root.py
+++ cspp1-practice/m5/p2/square_root.py
@@ -2,7 +2,7 @@
 def main():
     """using approximation method"""
     given_number = int(input())
-    epsilon = 0.01
+    epsilon = 0.001
     increment = 0.0001
     guess_value = 0.0
     while abs((guess_value ** 2) - given_number) >= epsilon and guess_value <= given_number:
