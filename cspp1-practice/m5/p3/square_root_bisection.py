c2f3b30b66cc2b82fb7371018c22b1ac38621db3
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-03 17:09:01 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- cspp1-practice/m5/p3/square_root_bisection.py
+++ cspp1-practice/m5/p3/square_root_bisection.py
@@ -1,13 +1,14 @@
 '"find the square root of the given number using Bi-section method "'
 
 def main():
+    """bisection method"""
     square = int(input())
-    epsilon = 0.0001
-    low = 1.0
+    epsilon = 0.01
+    low = 0
     high = square
     guess = (high + low)/2.0
     while abs(guess ** 2 - square) >= epsilon:
-        if guess ** 2 <= square:
+        if guess ** 2 < square:
             low = guess
         else:
             high = guess
