e5c71b76a4bd0ef3bae2a20cfca07f09d055eda9
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-04 10:27:49 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- cspp1-practice/m6/p3/digit_product.py
+++ cspp1-practice/m6/p3/digit_product.py
@@ -6,12 +6,24 @@ def main():
     Read any number from the input, store it in variable int_input.
     '''
     int_input = int(input())
-    digit_product = 1
-    while int_input != 0:
-        digit = int_input % 10
-        digit_product = digit_product * digit
-        int_input = int_input // 10
-    print(digit_product)
+	if int_input == 0:
+        print("0")
+    elif int_input < 0:
+        int_input = -int_input
+        digit_product = 1
+        while int_input != 0:
+            digit = int_input % 10
+            digit_product = digit_product * digit
+            int_input = int_input // 10
+        print(digit_product)
+    else:
+        int_input = -int_input
+        digit_product = 1
+        while int_input != 0:
+            digit = int_input % 10
+            digit_product = digit_product * digit
+            int_input = int_input // 10
+        print(digit_product)
 
 
 if __name__ == "__main__":
