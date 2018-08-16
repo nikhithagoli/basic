37fcda4eeedeaaca13ec445eb6558d0f8321a1b1
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-16 13:26:49 +0530

    " -> 15 of 19 passed. pylint: 9.48/10 "

--- cspp1-practice/m14/poker2/poker.py
+++ cspp1-practice/m14/poker2/poker.py
@@ -161,7 +161,7 @@ def hand_rank(hand):
         return 9
     elif is_four_of_a_kind(hand):
         return 8
-    elif is_three_of_a_kind(hand) and is_one_pair(hand):
+    elif is_full_house(hand):
         return 7
     elif is_flush(hand):
         return 6
