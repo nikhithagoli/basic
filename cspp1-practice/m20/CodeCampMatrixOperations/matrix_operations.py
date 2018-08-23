ace05a14271677c8cd4cc809b5a9175c6bd13ea6
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-23 09:58:28 +0530

    matrix operations

---
+++ cspp1-practice/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -0,0 +1,70 @@
+def mult_matrix(m1, m2):
+    '''
+        check if the matrix1 columns = matrix2 rows
+        mult the matrices and return the result matrix
+        print an error message if the matrix shapes are not valid for mult
+        and return None
+        error message should be "Error: Matrix shapes invalid for mult"
+    '''
+    
+    if len(m1[0]) == len(m2):
+    	multi_matrix = [[0 for j in range(len(m2[0]))] for i in range(len(m1))]
+    	for i in range(len(m1)):
+    		for j in range(len(m2[0])):
+    			for k in range(len(m2)):
+    				multi_matrix[i][j] = multi_matrix[i][j] + (m1[i][k] * m2[k][j])
+    	return multi_matrix
+    else:
+    	print('Error: Matrix shapes invalid for mult')
+    	return
+
+def add_matrix(m1, m2):
+    '''
+        check if the matrix shapes are similar
+        add the matrices and return the result matrix
+        print an error message if the matrix shapes are not valid for addition
+        and return None
+        error message should be "Error: Matrix shapes invalid for addition"
+    '''
+    added_matrix = []
+    if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
+    	for i in range(len(m1)):
+    		added_matrix.append([m1[i][j] + m2[i][j] for j in range(len(m1[0]))])
+    	return added_matrix 
+    else:
+    	print("Error: Matrix shapes invalid for addition")
+    	return
+
+def read_matrix(m,n):
+    '''
+        read the matrix dimensions from input
+        create a list of lists and read the numbers into it
+        in case there are not enough numbers given in the input
+        print an error message and return None
+        error message should be "Error: Invalid input for the matrix"
+    '''
+    matrix = []
+    for i in range(m):
+    	inp = input()
+    	if len(inp) == n+2 :
+    		matrix.append(list(map(int,inp.split(' '))))
+    	else:
+    		print("Error: Invalid input for the matrix")
+    return matrix
+
+
+
+def main():
+    # read matrix 1
+    matrix1_rows, matrix1_cols = map(int,input().split(','))
+    matrix1 = read_matrix(matrix1_rows,matrix1_cols)
+    # read matrix 2
+    matrix2_rows, matrix2_cols = map(int,input().split(','))
+    matrix2 = read_matrix(matrix2_rows,matrix2_cols)
+    # add matrix 1 and matrix 2
+    print(add_matrix(matrix1, matrix2))
+    # multiply matrix 1 and matrix 2
+    print(mult_matrix(matrix1,matrix2))
+
+if __name__ == '__main__':
+    main()
