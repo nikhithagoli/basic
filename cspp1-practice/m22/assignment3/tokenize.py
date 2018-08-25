f1a0faa7e6774e28494d61b6380db3d9421ca85c
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-25 10:54:53 +0530

    " -> 0 of 4 passed. pylint: 6.80/10 "

--- cspp1-practice/m22/assignment3/tokenize.py
+++ cspp1-practice/m22/assignment3/tokenize.py
@@ -2,9 +2,16 @@
 Write a function to tokenize a given string and return a dictionary with the frequency of
 each word
 '''
-
+def clean_string(string):
+    '''to remove spl characters'''
+    new_string = ""
+    for i in string:
+        if i not in "!@#$%^&*()-_+=:;.,? ":
+            new_string += i
+    return new_string
 def tokenize(string):
     tokens_dict = {}
+    string = clean_input(string)
     words_list = string.split(' ')
     for i in words_list:
         if i not in tokens_dict:
