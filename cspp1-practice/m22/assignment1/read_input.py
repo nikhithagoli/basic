2ea85c39a2297307edeefc2abc4008045a1a3c69
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-25 10:29:57 +0530

    m22

---
+++ cspp1-practice/m22/assignment1/read_input.py
@@ -0,0 +1,13 @@
+'''
+Write a python program to read multiple lines of text input and store the input into a string.
+'''
+
+def main():
+    text = ""
+    no_of_lines = int(input())
+    for i in range(no_of_lines):
+        text = text + input()
+        text = '\n'
+    print(text)
+if __name__ == '__main__':
+    main()
