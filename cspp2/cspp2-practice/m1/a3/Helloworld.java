8223462c3d77ec317feb3af5afcedd4362d5293c
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-27 22:58:07 +0530

    a3

--- cspp2/cspp2-practice/m1/a3/Helloworld.java
+++ cspp2/cspp2-practice/m1/a3/Helloworld.java
@@ -1,5 +1,22 @@
-public class Helloworld{
-	public static void main(String args[]){
-		System.out.println("hello world!");
-	}
+/**
+    * helloworld program.
+*/
+public final class Helloworld {
+    /**
+        * class.
+    */
+    private Helloworld() {
+        //function
+    }
+
+    /**
+        * @param args of the program.
+    */
+
+    public static void main(final String[] args) {
+        /**
+            main function.
+        */
+        System.out.println("Hello World");
+    }
 }
