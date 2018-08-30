86e9f95d39cda2c838b2df9f6c55f5743362d899
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-30 13:42:22 +0530

    m4- a11

--- cspp2/cspp2-practice/m4/a11/Average.java
+++ cspp2/cspp2-practice/m4/a11/Average.java
@@ -21,7 +21,7 @@ public final class Average {
         System.out.println("array length: ");
         int n = sc.nextInt();
         int[] a = new int[n];
-        int sum = 0; 
+        float sum = 0; 
         System.out.println("enter array elements: ");
         for(int i = 0; i < n;i++){
             a[i] = sc.nextInt();
