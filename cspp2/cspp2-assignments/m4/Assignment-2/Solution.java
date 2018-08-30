dda0a2bdc22254ef8d04fae6f6fb3eb6744e8ea1
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-30 15:16:00 +0530

    m4- assignment2

--- cspp2/cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2/cspp2-assignments/m4/Assignment-2/Solution.java
@@ -3,50 +3,54 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/** 
-	 * Fill the main function to print resultant of addition of matrices
-	 */
-	private Solution(){
-		//function.
-	}
 	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments.
-	 */
-	public static void main(final String[] args) {
+     * Fill the main function to print resultant of addition of matrices.
+    */
+    private Solution() {
+       //function.
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments.
+     */
+    public static void main(final String[] args) {
 		/**
-		 * { item_description }.
-		 */
-		int m,n,p,q;
-		Scanner sc = new Scanner(System.in);
-		m = sc.nextInt();
-		n = sc.nextInt();
-		p = sc.nextInt();
-		q = sc.nextInt();
-		int[][] a = new int[m][n];
-		int[][] b = new int[p][q];
-		for (int i = 0; i < m ; i++) {
-			for (int j = 0; j < n; j++) {
-				a[i][j] = sc.nextInt();
-			}
-		}
-		for (int i = 0; i < p ; i++) {
-			for (int j =0; j < q; j++) {
-				b[i][j] = sc.nextInt();
-			}
-		}				
-		if (m == p && n == q) {
-			int[][] c = new int[m][n];
-			for (int i = 0; i < m ; i++) {
-				for (int j =0; j < n; j++) {
-					c[i][j] = a[i][j] + b[i][j];
-					System.out.println(c[i][j]);
-				}
-			}
-		}
-		else {
-			System.out.println("not possible");
-		}
-	}
+         * { item_description }.
+         */
+        int m, n, p, q;
+        Scanner sc = new Scanner(System.in);
+        m = Integer.parseInt(sc.next());
+        n = Integer.parseInt(sc.next());
+        int[][] a = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = Integer.parseInt(sc.next());
+            }
+        }
+        p = Integer.parseInt(sc.next());
+        q = Integer.parseInt(sc.next());
+        int[][] b = new int[p][q];
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < q; j++) {
+                b[i][j] = Integer.parseInt(sc.next());
+            }
+        }
+        if (m == p && n == q) {
+            int[][] c = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    c[i][j] = a[i][j] + b[i][j];
+                }
+                String s = "";
+                for(int ele : c[i]) {
+                    s += Integer.toString(ele) + " ";
+                }
+                System.out.println(s);
+            }
+        } else {
+            System.out.println("not possible");
+        }
+
+    }
 }
