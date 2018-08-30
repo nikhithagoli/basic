34b0216c037828f3875848ac35bfbaaf638afea0
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-30 14:38:18 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- cspp2/cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2/cspp2-assignments/m4/Assignment-2/Solution.java
@@ -26,27 +26,27 @@ public final class Solution {
 		q = sc.nextInt();
 		int[][] a = new int[m][n];
 		int[][] b = new int[p][q];
-		for(int i = 0; i < m ; i++){
-			for(int j =0; j < n; j++){
+		for (int i = 0; i < m ; i++) {
+			for (int j = 0; j < n; j++) {
 				a[i][j] = sc.nextInt();
 			}
 		}
-		for(int i = 0; i < p ; i++){
-			for(int j =0; j < q; j++){
+		for (int i = 0; i < p ; i++) {
+			for (int j =0; j < q; j++) {
 				b[i][j] = sc.nextInt();
 			}
 		}				
-		if(m == p && n == q){
+		if (m == p && n == q) {
 			int[][] c = new int[m][n];
-			for(int i = 0; i < m ; i++){
-				for(int j =0; j < n; j++){
+			for (int i = 0; i < m ; i++) {
+				for (int j =0; j < n; j++) {
 					c[i][j] = a[i][j] + b[i][j];
 					System.out.println(c[i][j]);
 				}
 			}
 		}
-		else{
-			System.out.println("matrix addition is not possible");
+		else {
+			System.out.println("not possible");
 		}
 	}
 }
