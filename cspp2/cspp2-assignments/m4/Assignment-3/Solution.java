2b89885f7eb7dfd0e363849cb2014180b829ddb4
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-08-31 10:28:18 +0530

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

--- cspp2/cspp2-assignments/m4/Assignment-3/Solution.java
+++ cspp2/cspp2-assignments/m4/Assignment-3/Solution.java
@@ -5,48 +5,47 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	private Solution() {
-		//constructor.
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      s     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	static String binaryToDecimal(String s) {
-		/**
-		 * { item_description }.
-		 */
-		int sum = 0;
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == '1') {
-				sum += Math.pow(2, (s.length() - 1) - i);
-			}
-		}
-		return (Integer.toString(sum));
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		/**
-		 * { var_description }.
-		 */
-		Scanner sc = new Scanner(System.in);
-		String s;
-		int n = sc.nextInt();
-		for (int i = 1; i <= n; i++) {
-			s = sc.next();
-			String res = binaryToDecimal(s);
-			System.out.println(res);
-		}
-	}
-
+    /**
+    Do not modify this main function.
+    */
+    private Solution() {
+        //constructor.
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      s     { parameter_description }.
+     *
+     * @return     { description_of_the_return_value }.
+     */
+    static String binaryToDecimal(final String s) {
+        /**
+         * { item_description }.
+        */
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                sum += Math.pow(2, (s.length() - 1) - i);
+            }
+        }
+        return (Integer.toString(sum));
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }.
+        */
+        Scanner sc = new Scanner(System.in);
+        String s;
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            s = sc.next();
+            String res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
 }
