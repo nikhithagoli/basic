03ba9b5d5cbde61e291c4779608556afa205e7da
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-09-01 10:54:54 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2/cspp2-assignments/m6/Assignment-2/Solution.java
+++ cspp2/cspp2-assignments/m6/Assignment-2/Solution.java
@@ -6,70 +6,71 @@ import java.util.Scanner;
  * @author :
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//not used
-	}
-	/**
-	 * Function to round the
-	 * elements of a matrix to the nearest 100.
-	 *
-	 * @param      a     Matrix of which the elements to be rounded
-	 * @param      r     Number of rows
-	 * @param      c     Number of columns
-	 *
-	 * @return     Matrix of the rounded elements
-	 */
-	static int[][] round(final int[][] a, final int r, final int c) {
-
-		// write ypur code here
-		final int h = 100;
-		for (int i = 0; i < r; i++) {
-			for (int j = 0; j < c; j++) {
-				int num = a[i][j];
-				String s = Integer.toString(num);
-				if (num <= h) {
-					if (num < (h - num)) {
-						a[i][j] = 0;
-					} else {
-						a[i][j] = h;
-					}
-				} else {
-					String s1 = String.valueOf(s.charAt(0));
-					int n = Integer.parseInt(s1);
-					if ((num - (h * n)) < ((h * (n + 1)) - num)) {
-						a[i][j] = (h * n);
-					} else {
-						a[i][j] = (h * (n + 1));
-					}
-				}
-			}
-		}
-		return (a);
-	}
-	/**
-	 * Main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int m = scan.nextInt();
-		int n = scan.nextInt();
-		int[][] a = new int[m][n];
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				a[i][j] = scan.nextInt();
-			}
-		}
-		int[][] b = round(a, m, n);
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n - 1; j++) {
-				System.out.print(b[i][j] + " ");
-			}
-			System.out.println(b[i][n - 1]);
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //not used
+    }
+    /**
+     * Function to round the
+     * elements of a matrix to the nearest 100.
+     *
+     * @param      a     Matrix of which the elements to be rounded
+     * @param      r     Number of rows
+     * @param      c     Number of columns
+     *
+     * @return     Matrix of the rounded elements
+     */
+    static int[][] round(final int[][] a, final int r, final int c) {
+        // write ypur code here
+        final int h = 100;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                int num = a[i][j];
+                String s = Integer.toString(num);
+                if (num <= h) {
+                    if (num < (h - num)) {
+                        a[i][j] = 0;
+                    } else {
+                        a[i][j] = h;
+                    }
+                } else {
+                    String s1 = String.valueOf(s.charAt(0));
+                    int n = Integer.parseInt(s1);
+                    int upper = (h * (n + 1));
+                    int lower = (h * n);
+                    if ((num - lower) < ( upper - num)) {
+                        a[i][j] = lower;
+                    } else {
+                        a[i][j] = upper;
+                    }
+                }
+            }
+        }
+        return (a);
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] a = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = scan.nextInt();
+            }
+        }
+        int[][] b = round(a, m, n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n - 1; j++) {
+               System.out.print(b[i][j] + " ");
+            }
+            System.out.println(b[i][n - 1]);
+        }
+    }
 }
