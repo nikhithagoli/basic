f808a7ad5b8040b94e08df4dd1c8e839f6e4434b
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-09-01 10:48:26 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2/cspp2-assignments/m6/Assignment-2/Solution.java
+++ cspp2/cspp2-assignments/m6/Assignment-2/Solution.java
@@ -17,8 +17,8 @@ final class Solution {
 	 * elements of a matrix to the nearest 100.
 	 *
 	 * @param      a     Matrix of which the elements to be rounded
-	 * @param      rows     Number of rows
-	 * @param      columns     Number of columns
+	 * @param      r     Number of rows
+	 * @param      c     Number of columns
 	 *
 	 * @return     Matrix of the rounded elements
 	 */
@@ -37,7 +37,8 @@ final class Solution {
 						a[i][j] = h;
 					}
 				} else {
-					int n = Integer.parseInt(String.valueOf(s.charAt(0)));
+					String s1 = String.valueOf(s.charAt(0));
+					int n = Integer.parseInt(s1);
 					if ((num - (h * n)) < ((h * (n + 1)) - num)) {
 						a[i][j] = (h * n);
 					} else {
