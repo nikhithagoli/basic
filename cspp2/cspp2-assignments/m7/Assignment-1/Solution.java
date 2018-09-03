50de04b8de6af1bf27337c9893bbabc1934f2396
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-09-03 17:00:00 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2/cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2/cspp2-assignments/m7/Assignment-1/Solution.java
@@ -1,10 +1,9 @@
-import java.util.*;
+import java.util.Scanner;
 /**
  * Class for input validator.
- * 
+ *
  */
-class InputValidator
-{
+class InputValidator {
 	/*Write the atrributes and methods for InputValidator*/
 	int len;
 	/**
@@ -12,36 +11,34 @@ class InputValidator
 	 *
 	 * @param      str   The string
 	 */
-    InputValidator(String str){
-    	len = str.length();
-    }
-    /**
-     * { function_description }.
-     *
-     * @return     boolean value.
-     */
-    boolean validateData(){
-    	if (len >= 6){
-    		return (true);
-    	}
-        return (false);
-    }
+	InputValidator(String str) {
+		len = str.length();
+	}
+	/**
+	 * { function_description }.
+	 *
+	 * @return     boolean value.
+	 */
+	boolean validateData() {
+		if (len >= 6) {
+			return (true);
+		}
+		return (false);
+	}
 }
-public class Solution
-{
+public class Solution {
 	/**
 	 * main class.
 	 */
-	public static void main(String args[])
-    {
-    	/**
-    	 * { var_description }.
-    	 */
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);    	
-    	System.out.println(i.validateData());
+	public static void main(String args[]) {
+		/**
+		 * { var_description }.
+		 */
+		Scanner s = new Scanner(System.in);
+		String input = s.next();
+		InputValidator i = new InputValidator(input);
+		System.out.println(i.validateData());
 
-    }
+	}
 
 }
