97d5a10da9bde2eb05b42af20fef0403e4ca9aa9
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-09-03 16:31:36 +0530

    m7-a11

---
+++ cspp2/cspp2-practice/m7/Student.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.lang.*;
+/**
+ * to find average GPA.
+*/
+public class Student {
+    /**
+        * class.
+    */
+    String name, rollnumber;
+    void gpa(double a, double b, double c) {
+        System.out.println((double)((int)(((a + b + c) / 3)*10.0))/10.0);
+    }
+    Student(String name, String rollnumber, double a, double b, double c) {
+        //function
+        this.name = name;
+        this.rollnumber = rollnumber;
+        gpa(a,b,c);
+    }
+    /**
+        * @param args of the program.
+    */
+    public static void main(final String[] args) {
+        /**
+           * main function.
+        */
+        Scanner sc = new Scanner(System.in);
+        Student s = new Student(sc.next(), sc.next(), sc.nextDouble(), sc.nextDouble(), sc.nextDouble());
+    }
+}
+
