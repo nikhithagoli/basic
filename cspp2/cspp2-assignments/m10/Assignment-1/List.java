a370666799b48bbaff8392329213ed425bfbc28b
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-09-06 11:05:56 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2/cspp2-assignments/m10/Assignment-1/List.java
+++ cspp2/cspp2-assignments/m10/Assignment-1/List.java
@@ -294,8 +294,9 @@ public class List {
      */
     public int indexOf(int item) {
         for (int i = 0; i < size; i++) {
-            if (item == list[i])
+            if (item == list[i]){
                 return i;
+            }
         }
         return -1;
     }
@@ -334,8 +335,9 @@ public class List {
         // write the logic
         int c = 0;
         for (int element : list) {
-            if (element == item)
+            if (element == item){
                 c++;
+            }
         }
         return c;
     }
@@ -361,8 +363,9 @@ public class List {
                     if (t.length == 1) {
                         l.add(Integer.parseInt(tokens[1]));
                     } else {
-                        if (t.length > 1)
+                        if (t.length > 1){
                             l.add(Integer.parseInt(t[0]), Integer.parseInt(t[1]));
+                        }
                     }
                 }
                 break;
@@ -372,9 +375,10 @@ public class List {
             case "addAll":
                 if (tokens.length == 2) {
                     String[] t1 = tokens[1].split(",");
-                    int temp[] = new int[t1.length];
-                    for (int i = 0; i < temp.length; i++)
+                    int[] temp = new int[t1.length];
+                    for (int i = 0; i < temp.length; i++){
                         temp[i] = Integer.parseInt(t1[i]);
+                    }
                     l.addAll(temp);
                 }
                 break;
@@ -401,6 +405,8 @@ public class List {
                 break;
             case "contains":
                 System.out.println(l.contains(Integer.parseInt(tokens[1])));
+                break;
+            default :
                 break;
             }
         }
