653f9e01c2599f1210b2b1bf92d0abfdb492d1a0
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-09-07 11:55:59 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2/cspp2-assignments/m11/Assignment-1/List.java
+++ cspp2/cspp2-assignments/m11/Assignment-1/List.java
@@ -216,13 +216,14 @@ public class List {
     public void removeAll(int[] newArray) {
         // write the logic
         for (int each : newArray) {
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i <= size; i++) {
                 if (each == list[i]) {
                     remove(i);
 
                 }
             }
         }
+
 
     }
     /*
