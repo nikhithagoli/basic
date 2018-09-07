cc550ea4b0a232ba2bb46f5eab7c77d11c66ee90
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-09-07 11:49:08 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2/cspp2-assignments/m11/Assignment-1/List.java
+++ cspp2/cspp2-assignments/m11/Assignment-1/List.java
@@ -117,7 +117,7 @@ public class List {
         // write the logic for remove here. Think about what to do to the size
         // variable.
         if (index >= 0 && index < size && size >= 0) {
-            for (int i = index; i < size; i++) {
+            for (int i = index; i < size - 1; i++) {
                 list[i] = list[i + 1];
             }
             size--;
