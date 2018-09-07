b017058200e13b4e08f54c65ef406168450aaebf
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-09-07 12:32:59 +0530

    "check_style_score -> 3 of 3 passed. style score: 1/1 "

--- cspp2/cspp2-assignments/m11/Assignment-1/List.java
+++ cspp2/cspp2-assignments/m11/Assignment-1/List.java
@@ -275,6 +275,7 @@ public class List {
             for (int i = 0; i < size; i++) {
                 if (each == list[i]) {
                     remove(i);
+                    i = 0;
 
                 }
             }
