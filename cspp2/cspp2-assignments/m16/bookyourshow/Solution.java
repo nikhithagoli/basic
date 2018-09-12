4fca5d42cccb06e4e3a242110a9da0d58265e31e
nikhithagoli <nikhithagolinikki86@msitprogram.net>
2018-09-12 18:11:07 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2/cspp2-assignments/m16/bookyourshow/Solution.java
+++ cspp2/cspp2-assignments/m16/bookyourshow/Solution.java
@@ -253,8 +253,8 @@ public final class Solution {
                 break;
             case "book":
                 elements = tokens[1].split(",", k + 2);
-                s = (elements[k + 2].
-                     substring(1, elements[k + 2].length() - 1)).split(",");
+                s = (elements[k + 1].
+                     substring(1, elements[k + 1].length() - 1)).split(",");
                 b.bookAShow(elements[0], elements[1],
                             new Patron(elements[2], elements[3]), s);
                 break;
